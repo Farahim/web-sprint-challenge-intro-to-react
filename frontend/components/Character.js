ffa06e06c1d54d830d2525df4a4c4fe5e737ba03
Farahim <farahim@hotmail.com>
2024-05-17 23:56:50 -0400

    update

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
+
+function Character(props) {
+  const {character} = props;
+  const [planetToggle, setPlanetToggle] = useState(false);
+
+  const toggle= () => {
+    setPlanetToggle(!planetToggle);
+  }
 
 function Character() { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
@@ -6,7 +15,32 @@ function Character() { // ❗ Add the props
   return (
     <div>
       {/* Use the same markup with the same attributes as in the mock */}
-    </div>
+
+      <div className="character-card" onClick={(e) => toggle()}>
+        <h3 className="character-name">{character.name}</h3>
+        <p>Birth_year: {character.birth_year}</p>
+        <p>Eye_color: {character.eye_color}</p>
+        <p>Gender: {character.gender}</p>
+        <p>Hair_color: {character.hair_color}</p>
+        <p>Height: {character.height}</p>
+        <p>Mass: {character.mass}</p>
+        <p>Skin_color: {character.skin_color}</p>
+        {
+          planetToggle?
+              <div className="character-planet">
+                <h3>{character.homeworld.name}</h3>
+                <p>Climate: {character.homeworld.climate}</p>
+                <p>Diameter: {character.homeworld.diameter}</p>
+                <p>Gravity: {character.homeworld.gravity}</p>
+                <p>Orbital_period: {character.homeworld.orbital_period}</p>
+                <p>Population: {character.homeworld.population}</p>
+                <p>Rotation_period: {character.homeworld.rotation_period}</p>
+                <p>Surface_water: {character.homeworld.surface_water}</p>
+                <p>Terrain: {character.homeworld.terrain}</p>
+              </div>
+        }
+
+          </div>
   )
 }
 
